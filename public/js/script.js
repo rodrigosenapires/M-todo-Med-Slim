a377dacb5dbd8a619828ebd6401e185d6d275865
rodrigo <rodrigosenapires86@gmail.com>
2023-03-06 12:46:46 -0300

    Primeiro Commit

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// Botão E-mail
+var enviarEmail = document.getElementById('enviarEmail');
+enviarEmail.addEventListener('click', function() {
+  window.location.href = 'mailto:contato@receitassaudaveis.site';
+});
+
+
+
+
+
+
+
+
