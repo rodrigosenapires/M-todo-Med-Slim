6885dd55eb55c6c2ce0379484a19ffbad47b1a1b
rodrigo <rodrigosenapires86@gmail.com>
2023-03-23 21:42:23 -0300

    Teste da notificação de vendas3

--- public/js/notificacao-vendas.js
+++ public/js/notificacao-vendas.js
@@ -3,20 +3,34 @@ function numeroAleatorio(min, max) {
 }
 
 let ultimaVenda = 0;
+let contadorNotificacoes = 0;
 
 function exibirNotificacao() {
     const notificacao = document.getElementById('notificacao');
-    const vendas = numeroAleatorio(ultimaVenda + 1, 128);
+    let vendas;
+    
+    if (contadorNotificacoes === 0) {
+        vendas = numeroAleatorio(28, 32);
+    } else if (contadorNotificacoes === 1) {
+        vendas = numeroAleatorio(33, 39);
+    } else if (contadorNotificacoes === 2) {
+        vendas = numeroAleatorio(39, 43);
+    } else if (contadorNotificacoes === 3) {
+        vendas = numeroAleatorio(43, 53);
+    } else {
+        vendas = ultimaVenda + 7;
+    }
 
     notificacao.textContent = `${vendas} já compraram esse curso hoje!`;
     notificacao.style.display = 'block';
 
     ultimaVenda = vendas;
+    contadorNotificacoes++;
 
     setTimeout(() => {
         notificacao.style.display = 'none';
     }, 5000);
 }
 
-setInterval(exibirNotificacao, 3 * 60 * 1000);
+setInterval(exibirNotificacao, 1 * 60 * 1000);
 exibirNotificacao();
