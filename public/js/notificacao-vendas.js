b8198f42688132972b1b83c976feae49cc4e5e9e
rodrigo <rodrigosenapires86@gmail.com>
2023-03-23 22:01:37 -0300

    Teste da notificação de vendas4

--- public/js/notificacao-vendas.js
+++ public/js/notificacao-vendas.js
@@ -2,14 +2,34 @@ function numeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let ultimaVenda = 0;
-let contadorNotificacoes = 0;
+function armazenarUltimaVenda(vendas) {
+    const dataExpiracao = new Date().getTime() + 24 * 60 * 60 * 1000;
+    const vendaData = {
+        valor: vendas,
+        expiracao: dataExpiracao
+    };
+    localStorage.setItem('ultimaVenda', JSON.stringify(vendaData));
+}
+
+function obterUltimaVenda() {
+    const vendaData = JSON.parse(localStorage.getItem('ultimaVenda'));
+    if (vendaData && vendaData.expiracao > new Date().getTime()) {
+        return vendaData.valor;
+    }
+    return 0;
+}
+
+let ultimaVenda = obterUltimaVenda();
+let contadorNotificacoes = ultimaVenda === 0 ? 0 : 4;
 
 function exibirNotificacao() {
     const notificacao = document.getElementById('notificacao');
     let vendas;
-    
-    if (contadorNotificacoes === 0) {
+
+    if (ultimaVenda !== 0) {
+        vendas = ultimaVenda + 9;
+        ultimaVenda = 0;
+    } else if (contadorNotificacoes === 0) {
         vendas = numeroAleatorio(28, 32);
     } else if (contadorNotificacoes === 1) {
         vendas = numeroAleatorio(33, 39);
@@ -26,6 +46,7 @@ function exibirNotificacao() {
 
     ultimaVenda = vendas;
     contadorNotificacoes++;
+    armazenarUltimaVenda(vendas);
 
     setTimeout(() => {
         notificacao.style.display = 'none';
