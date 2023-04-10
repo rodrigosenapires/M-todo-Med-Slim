cd28d82340d4f77b60954496da05c533bc167024
rodrigo <rodrigosenapires86@gmail.com>
2023-04-10 08:43:24 -0300

    Refiz a loja

--- public/js/custom-store.js
+++ public/js/custom-store.js
@@ -1,49 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var categoryButtons = document.querySelectorAll('.category-selection li, #rodapeCategorias a');
-    var products = document.querySelectorAll('.product');
-    var footerCategoryButtons = document.querySelectorAll('#rodapeCategorias a');
-
+document.addEventListener("DOMContentLoaded", function () {
+    var categoryButtons = document.querySelectorAll("#listaCategoriasProdutos li");
+    var products = document.querySelectorAll(".product");
+  
     showAllProducts();
-
-    categoryButtons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            var selectedCategory = this.getAttribute('data-category') || this.getAttribute('href').split('#')[1].toLowerCase();
-            filterProducts(selectedCategory);
-        });
+  
+    categoryButtons.forEach(function (button) {
+      button.addEventListener("click", function (event) {
+        event.preventDefault();
+        var selectedCategory = this.getAttribute("data-category");
+        filterProducts(selectedCategory);
+      });
     });
-
-    footerCategoryButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            scrollToTop();
-        });
-    });
-
-    var allButton = document.querySelector('[data-category="all"]');
-    allButton.addEventListener('click', function() {
-        showAllProducts();
-    });
-
+  
     function showAllProducts() {
-        products.forEach(function(product) {
-            product.classList.add('show');
-        });
+      products.forEach(function (product) {
+        product.style.display = "block";
+      });
     }
-
+  
     function filterProducts(category) {
-        products.forEach(function(product) {
-            if (product.getAttribute('data-category') === category) {
-                product.classList.add('show');
-            } else {
-                product.classList.remove('show');
-            }
-        });
+      if (category === "all") {
+        showAllProducts();
+        return;
+      }
+  
+      products.forEach(function (product) {
+        if (product.getAttribute("data-category") === category) {
+          product.style.display = "block";
+        } else {
+          product.style.display = "none";
+        }
+      });
     }
-
-    function scrollToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    }
-});
+  });
+  
