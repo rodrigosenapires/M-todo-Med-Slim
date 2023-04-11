4af38c46521fed5c35a5841157857441fcaebc6c
rodrigo <rodrigosenapires86@gmail.com>
2023-04-10 23:39:48 -0300

    inserção do vídeo de fundo no index2

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,14 +1,29 @@
 const audioControl = document.getElementById("audio-control");
 const bgVideo = document.getElementById("bg-video");
 
+function onYouTubeIframeAPIReady() {
+  bgVideo.player = new YT.Player("bg-video", {
+    events: {
+      onReady: function (event) {
+        event.target.setVolume(0);
+      }
+    }
+  });
+}
+
 audioControl.addEventListener("click", () => {
-  if (bgVideo.muted) {
-    bgVideo.muted = false;
+  if (bgVideo.player.isMuted()) {
+    bgVideo.player.unMute();
     audioControl.classList.remove("fa-volume-mute");
     audioControl.classList.add("fa-volume-up");
   } else {
-    bgVideo.muted = true;
+    bgVideo.player.mute();
     audioControl.classList.remove("fa-volume-up");
     audioControl.classList.add("fa-volume-mute");
   }
 });
+
+const tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
