ff28e7ef51367826d36c3d1e6642ee87cd803877
rodrigo <rodrigosenapires86@gmail.com>
2023-04-13 22:57:40 -0300

    Ajustes da página de agradecimento por baixae o ebook grátis13

--- public/js/carrossel.js
+++ public/js/carrossel.js
@@ -1,9 +1,41 @@
-document.querySelector(".vejaMais").addEventListener("click", function() {
-    const carrosselContainer = document.querySelector(".carrossel-container");
+const vejaMais = document.querySelector(".vejaMais");
+const carrosselContainer = document.querySelector(".carrossel-container");
+const carrossel = document.querySelector(".carrossel");
+const carrosselItems = document.querySelectorAll(".carrossel-item");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const fecharCarrossel = document.querySelector(".fechar-carrossel");
+
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    carrosselItems.forEach(item => {
+        item.style.display = "none";
+    });
+    carrosselItems[slideIndex].style.display = "block";
+}
+
+vejaMais.addEventListener("click", () => {
     carrosselContainer.style.display = "flex";
+    showSlide(currentSlide);
 });
 
-document.querySelector(".carrossel-container").addEventListener("click", function() {
-    const carrosselContainer = document.querySelector(".carrossel-container");
+arrowLeft.addEventListener("click", () => {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = carrosselItems.length - 1;
+    }
+    showSlide(currentSlide);
+});
+
+arrowRight.addEventListener("click", () => {
+    currentSlide++;
+    if (currentSlide >= carrosselItems.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+});
+
+fecharCarrossel.addEventListener("click", () => {
     carrosselContainer.style.display = "none";
 });
